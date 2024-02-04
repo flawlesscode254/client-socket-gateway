3fd4d0a7d714a223eb51dad503a30ea010d5d173
flawlesscode254 <denzme414@gmail.com>
2024-02-04 21:38:59 +0300

    Done

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
 class gateway {
-    static getResponse(url, reference) {
-      return new Promise((resolve, reject) => {
-        const ws = new WebSocket(url);
-  
-        ws.onopen = () => {
-          setInterval(ping, 30000);
-        };
-  
-        const ping = () => {
-          ws.send(JSON.stringify({ ping: 1 }));
-        };
-  
-        ws.addEventListener("message", (event) => {
-          const data = JSON.parse(event.data);
-          const { id, content } = data;
-          if (id === reference) {
-            resolve(content);
-            ws.close();
-          }
-        });
+  static getResponse(url, reference) {
+    return new Promise((resolve, reject) => {
+      const ws = new WebSocket(url);
+
+      ws.addEventListener("message", (event) => {
+        const data = JSON.parse(event.data);
+        const { id, content } = data;
+        if (id === reference) {
+          resolve(content);
+          ws.close();
+        }
       });
-    }
+    });
   }
-  
-  export { gateway };
-  
+}
+
+export { gateway };
